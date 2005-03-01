0b32d1313c4d4731b601bf5a8780bf373c386505
Ilja Booij <ilja@ic-s.nl>
2005-03-01 14:53:58 +0000

    file permissions are now checked on the cache directory

--- util.c
+++ util.c
@@ -9,7 +9,19 @@
 #include "http_config.h"
 #include "http_protocol.h"
 
+#include "apr_file_info.h"
+
 int util_file_is_writable(apr_pool_t *p, const char *path)
 {
- 	return 0;
+	apr_finfo_t file_info;
+	apr_uid_t user_id;
+	apr_gid_t group_id;
+	
+	apr_stat(&file_info, path, APR_FINFO_USER | APR_FINFO_GROUP |
+		APR_FINFO_PROT, p);
+	apr_uid_current(&user_id, &group_id, p);
+	
+	if (file_info.protection & APR_UWRITE)
+	 	return 1;
+	 return 0;
 }
